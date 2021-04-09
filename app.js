e009087f4b2f0493d2f72a7364833abe52cea8f2
gaiyadev <gaiyaobed94@gmail.com>
2021-04-09 16:50:58 +0100

    buf

--- app.js
+++ app.js
@@ -10,15 +10,14 @@ require("dotenv").config();
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/api/users");
 var notesRouter = require("./routes/api/notes");
-app.use(cors());
 
-
+var app = express();
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-var app = express();
+app.use(cors());
 
 app.use("/", indexRouter);
 app.use("/api/users", usersRouter);
